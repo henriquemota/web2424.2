7e2bfd957dd35dc5dddd71fab88d9432c7121435
henriquemota <mota.henrique@gmail.com>
2024-09-25 09:52:36 -0300

    inicializando react

---
+++ react/hello/src/app.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div>
+			<h1 className='bg-info'>Bem vindo ao ReactJS</h1>
+			<button className='btn btn-success'>botao aqui</button>
+			<button className='btn btn-danger'>botao aqui</button>
+		</div>
+	)
+}
+
+export default App
