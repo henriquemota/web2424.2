001ce9c1453c46760ec116578709d99b2dd00d6e
henriquemota <mota.henrique@gmail.com>
2024-10-09 08:16:37 -0300

    implementacao da listagem de dados

--- react/dados/src/app.jsx
+++ react/dados/src/app.jsx
@@ -1,5 +1,31 @@
+import { alunos } from './data'
+
 function App() {
-	return <main></main>
+	return (
+		<main className='p-4'>
+			<h1>Lista de alunos</h1>
+			<table className='border border-gray-600 w-full'>
+				<thead>
+					<tr className='border border-gray-600'>
+						<th>Nome</th>
+						<th>AV1</th>
+						<th>AV2</th>
+						<th>AV3</th>
+					</tr>
+				</thead>
+				<tbody>
+					{alunos.map((el, ix) => (
+						<tr key={ix} className='border border-gray-600'>
+							<td>{el.nome}</td>
+							<td className='text-center'>{el.av1.toFixed(1)}</td>
+							<td className='text-center'>{el.av2.toFixed(1)}</td>
+							<td className='text-center'>{el.av3.toFixed(1)}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</main>
+	)
 }
 
 export default App
